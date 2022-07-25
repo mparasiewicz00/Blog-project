72b8c9a4377b0a5e8e76a8fd69d7e4aadffac142
Mateusz Parasiewicz <m.parasiewicz00@gmail.com>
2022-07-25 17:01:42 +0200

    Daily update

--- includes/db.php
+++ includes/db.php
@@ -4,16 +4,18 @@ $db['db_user'] = "root";
 $db['db_pass'] = "";
 $db['db_name'] = "blog_project";
 
-foreach ($db as $key => $value){
-define(strtoupper($key), $value);
+foreach ($db as $key => $value) {
+    define(strtoupper($key), $value);
 }
 
-$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-if(!$connection) {
-    die("Connecting failed");
-}
-else{
-    echo "Connected.";
-}
+    $connection = mysqli_connect(DB_HOST,DB_USER,
+        DB_PASS,DB_NAME);
+
+//if(!$connection) {
+//    die("Connecting failed");
+//}
+//else{
+//    echo "Connected.";
+//}
 
 ?>
