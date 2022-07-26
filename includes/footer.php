7b676db5ed2836d19115a107521cc842d60ebf1f
Mateusz Parasiewicz <m.parasiewicz00@gmail.com>
2022-07-26 13:02:58 +0200

    Updating adding new post from with data from DB

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
 <footer>
     <div class="row">
         <div class="col-lg-12">
-            <p>Copyright &copy; Your Website 2014</p>
+            <p>Copyright &copy; Mateusz Parasiewicz 2022</p>
         </div>
         <!-- /.col-lg-12 -->
     </div>
--- index.php
+++ index.php
@@ -5,8 +5,7 @@
 <!-- Navigation -->
 <?php include "includes/navigation.php";?>
 
-    <!-- Page Content -->
-    <div class="container">
+    <!-- Page Content --> <div class="container">
 
         <div class="row">
 
@@ -15,6 +14,19 @@
 
             <div class="col-md-8">
 
+                <?php
+                global $connection;
+                $query = "SELECT * FROM posts";
+                $select_all_posts_query = mysqli_query($connection, $query);
+
+                while($row = mysqli_fetch_assoc($select_all_posts_query)){
+                    $post_title = $row['post_title'];
+                    $post_author = $row['post_author'];
+                    $post_date= $row['post_date'];
+                    $post_image = $row['post_image'];
+                    $post_content = $row['post_content'];
+                    ?>
+
                 <h1 class="page-header">
                     Page Heading
                     <small>Secondary Text</small>
@@ -22,20 +34,21 @@
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#">Blog Post Title</a>
+                    <a href="#"><?php echo $post_title?></a>
                 </h2>
                 <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
+                    by <a href="index.php"><?php echo $post_author?></a>
                 </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
+                <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date?></p>
                 <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                <img class="img-responsive" src="https://via.placeholder.com/900x150.png/d3d3d3/0000?text=Basic+image+here" alt="">
                 <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
+                <p><?php echo $post_content?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
 
+                <?php } ?>
 
 
             </div>
