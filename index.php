631b4e439617936d8a298257b8d78e6fbb7fd9df
Mateusz Parasiewicz <m.parasiewicz00@gmail.com>
2022-07-27 14:53:10 +0200

    Adding display images in post and beggining of search engine

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date?></p>
                 <hr>
-                <img class="img-responsive" src="https://via.placeholder.com/900x150.png/d3d3d3/0000?text=Basic+image+here" alt="">
+                <img class="img-responsive" src="images/<?php echo $post_image;?>" alt="">
                 <hr>
                 <p><?php echo $post_content?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
