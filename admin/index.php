96eee042383be9bf0e607ba3241f1763e3fdb26e
Mateusz Parasiewicz <m.parasiewicz00@gmail.com>
2022-07-29 16:39:48 +0200

    Changes in sidebar admmin menu

--- admin/index.php
+++ admin/index.php
@@ -14,7 +14,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Hello, admin!
+                            Welcome
                             <small>Author</small>
                         </h1>
                     </div>
