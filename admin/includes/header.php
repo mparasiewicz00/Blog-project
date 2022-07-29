1d81c4301c3a40bfa1464c9ceb3ec65f56ddcceb
Mateusz Parasiewicz <m.parasiewicz00@gmail.com>
2022-07-29 16:49:18 +0200

    Daily changes

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -8,7 +8,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-
     <title>SB Admin - Bootstrap Admin Template</title>
 
     <!-- Bootstrap Core CSS -->
